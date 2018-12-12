52eea2b46929060f279d3fa62969a3d0e4018a24
ab55676 <ab55676@XIN009CDAB55676.ctl.intranet>
2018-12-12 18:13:54 +0530

    added some code

--- src/com/kd/concurrent/ConcurrentModificationExceptionDemo.java
+++ src/com/kd/concurrent/ConcurrentModificationExceptionDemo.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 /**
  * @author Kumar
  *
+ *         Output: aaa Child thread : Thread-0 executing adding in list
+ *         Exception in thread "main"
+ *         java.util.ConcurrentModificationException...
  *
- *Output:
- *		aaa
- *		Child thread : Thread-0 executing adding in list
- *  	Exception in thread "main" java.util.ConcurrentModificationException...
- *
- *
+ *         Definition: If one thread iterating/modifying an object and other
+ *         thread is trying to modify same abject then
+ *         ConcurrentModificationException occurs.
  *
  */
 public class ConcurrentModificationExceptionDemo {
@@ -22,15 +22,21 @@ public class ConcurrentModificationExceptionDemo {
 
 		@Override
 		public void run() {
-			try {
-				Thread.sleep(2000);
-			} catch (Exception e) {
+
+			for (String str : list) {
+				System.out.println("Child thread : " + Thread.currentThread().getName() + " executing adding in list");
+				System.out.println(str);
+				try {
+					Thread.sleep(2000);
+				} catch (Exception e) {
+				}
+				list.add("eee");
 			}
-			System.out.println("Child thread : " + Thread.currentThread().getName() + " executing adding in list");
-			list.add("eee");
+
+			//
 		}
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
 
 		list.add("aaa");
@@ -38,15 +44,19 @@ public class ConcurrentModificationExceptionDemo {
 		list.add("ccc");
 		list.add("ddd");
 
-		Thread t1 = new Thread( new Task());
+		Thread t1 = new Thread(new Task());
 		t1.start();
-		
+
 		for (String str : list) {
 			System.out.println(str);
 			Thread.sleep(2000);
 		}
+		
+		/*for(int i=0; i < 2; i++) {
+			list.add("a");
+			Thread.sleep(2000);
+		}*/
 
 	}
 
-	
 }
